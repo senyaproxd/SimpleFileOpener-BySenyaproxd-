f034785cf13ee397148c19ce25e5effd4b8fc767
Arseniy <senyaproxd@mail.ru>
2026-02-17 12:30:52 +0300

    New file added.
    Added the project file after changing readme file

---
+++ SimpleFileOpener(BySenyaproxd).py
@@ -0,0 +1,6 @@
+print("Type the file repository to open it(EXAMPLE C:|Users|[Your Name])Or copy and paste the file repository via widnows functions!")
+import os
+file_path = input()
+os.startfile(file_path)
+
+#The test project made by senyaproxd(expect changes!).
