320161d31bc79cda8b71f88b6c317a4f13db5b1f
Arseniy <senyaproxd@mail.ru>
2026-02-18 12:18:25 +0300

    New update!
    Added notifications if the input is empty and small status of file.

--- SimpleFileOpener(BySenyaproxd).py
+++
@@ -1,6 +0,0 @@
-print("Type the file repository to open it(EXAMPLE C:|Users|[Your Name])Or copy and paste the file repository via widnows functions!")
-import os
-file_path = input()
-os.startfile(file_path)
-
-#The test project made by senyaproxd(expect changes!).
---
+++ SimpleFileOpener(BySenyaproxd)v0.2.py
@@ -0,0 +1,10 @@
+print("Type the file repository to open it(EXAMPLE C:\\Users\\[Your Name])Or copy and paste the file repository via windows functions!")
+import os
+file_path = input()
+os.startfile(file_path)
+if os.path.exists:
+    print("The file is opened.")
+if file_path == "":
+   print("The input is empty(This will open the python file repository! Or pycharm projects!")
+
+#The test project made by senyaproxd(expect changes!)Version 0.2 (previous was not named but v0.1).
